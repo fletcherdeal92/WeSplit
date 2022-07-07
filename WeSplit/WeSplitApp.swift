d477a8ca3b52a7daeeae5e3441b448ae453e2d77
fletcherdeal92 <fletcherdeal@gmail.com>
2022-07-07 17:46:02 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Fletcher Deal on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
